7473a26fb9ad6a86778f2ccbb143a8cb747d5c17
Max <7087907+maxpromer@users.noreply.github.com>
2025-03-17 03:28:51 +0700

    Update SquareLine Studio Project

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,18 @@
 #include <Arduino.h>
 #include <lvgl.h>
-#include <ATD3.5-S3.h>
+#include <ATD-S3_Driver.h>
 #include "gui/ui.h"
-
-#define LED_Y_PIN (5)
 
 void setup() {
   Serial.begin(115200);
   
   // Setup peripherals
-  Display.begin(0); // rotation number 0
+  Display.begin(LCD_SIZE_800x480, 0); // rotation number 0
   Touch.begin();
-  Sound.begin();
-  // Card.begin(); // uncomment if you want to Read/Write/Play/Load file in MicroSD Card
-  pinMode(LED_Y_PIN, OUTPUT);
-  digitalWrite(LED_Y_PIN, HIGH); // off LED Y
-  
+
   // Map peripheral to LVGL
   Display.useLVGL(); // Map display to LVGL
   Touch.useLVGL(); // Map touch screen to LVGL
-  Sound.useLVGL(); // Map speaker to LVGL
-  // Card.useLVGL(); // Map MicroSD Card to LVGL File System
 
   Display.enableAutoSleep(120); // Auto off display if not touch in 2 min
   
@@ -28,12 +20,12 @@ void setup() {
   ui_init();
 
   // Add event handle
-  lv_obj_add_event_cb(ui_led_switch, [](lv_event_t * e) { // Add click LED switch event handle
+  lv_obj_add_event_cb(ui_test_switch, [](lv_event_t * e) { // Add click LED switch event handle
     lv_obj_t * target = lv_event_get_target(e);
     if (lv_obj_has_state(target, LV_STATE_CHECKED)) { // if switch on
-      digitalWrite(LED_Y_PIN, LOW); // ON LED Y
+      lv_obj_set_style_text_color(ui_test_switch_label, lv_color_hex(0x000000), LV_PART_MAIN | LV_STATE_DEFAULT); // set text color to black
     } else {
-      digitalWrite(LED_Y_PIN, HIGH); // OFF LED Y
+      lv_obj_set_style_text_color(ui_test_switch_label, lv_color_hex(0xFF9600), LV_PART_MAIN | LV_STATE_DEFAULT); // set text color to orange
     }
   }, LV_EVENT_CLICKED, NULL);
 }
